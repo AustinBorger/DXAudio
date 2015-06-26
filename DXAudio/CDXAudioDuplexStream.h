e6e4466985eb3f611cd57905cf21e4caeb33bc23
Austin Borger <aaborger@gmail.com>
2015-06-26 12:57:27 -0400

    CDXAudioDuplexStream.h comments

--- DXAudio/CDXAudioDuplexStream.h
+++ DXAudio/CDXAudioDuplexStream.h
@@ -30,6 +30,8 @@
 #include "ClientReader.h"
 #include "ClientWriter.h"
 
+/* This class is a final implementation of IDXAudioStream.  It is used for streams
+** that both read/write data to/from the default audio input/output endpoints. */
 class CDXAudioDuplexStream : public CDXAudioStream {
 public:
 	CDXAudioDuplexStream();
@@ -38,41 +40,56 @@ public:
 
 	//IDXAudioStream methods
 
+	/* Returns the type of the stream */
 	DXAUDIO_STREAM_TYPE STDMETHODCALLTYPE GetStreamType() final {
 		return DXAUDIO_STREAM_TYPE_DUPLEX;
 	}
 
 	//CDXAudioStream methods
 
+	/* Initializes all stream objects and data */
 	void ImplInitialize() final;
 
+	/* Calls Start() on the client */
 	void ImplStart() final;
 
+	/* Calls Stop() on the client */
 	void ImplStop() final;
 
+	/* Reacts to a default device change by checking to see if the held device is no longer default,
+	** then re-initializes to the new default device */
 	void ImplDeviceChange() final;
 
+	/* Reacts to a change in an endpoint property value by checking to see if the client is still valid -
+	** if not, the client is re-initialized */
 	void ImplPropertyChange() final;
 
+	/* Reads data from the input stream, calls Process() on the callback object, then writes the given data
+	** to the output stream. */
 	void ImplProcess() final;
 
 	//New methods
 
+	/* Checks to see if the callback is valid, then calls CDXAudioStream::Initialize() */
 	HRESULT Initialize(FLOAT SampleRate, IDXAudioCallback* pDXAudioCallback);
 
 private:
-	CComPtr<IMMDevice> m_InputDevice;
-	CComPtr<IMMDevice> m_OutputDevice;
-	CComPtr<IDXAudioReadWriteCallback> m_ReadWriteCallback;
-	LPWSTR m_InputDeviceID;
-	LPWSTR m_OutputDeviceID;
-	ClientReader m_ClientReader;
-	ClientWriter m_ClientWriter;
-	bool m_Running;
+	CComPtr<IMMDevice> m_InputDevice; //The device we're reading from
+	CComPtr<IMMDevice> m_OutputDevice; //The device we're writing to
+	CComPtr<IDXAudioReadWriteCallback> m_ReadWriteCallback; //The callback object
+	LPWSTR m_InputDeviceID; //The input device's unique identifier
+	LPWSTR m_OutputDeviceID; //The output device's unique identifier
+	ClientReader m_ClientReader; //Used for reading input data from the stream
+	ClientWriter m_ClientWriter; //Used for writing output data to the stream
+	bool m_Running; //Indicates whether or not the stream is running (used for routing)
 
+	/* Initializes the client reader object */
 	void InitClientReader();
 
+	/* Initializes the client writer object */
 	void InitClientWriter();
 
+	/* Responds to an HRESULT - if there is a failure, it will call the OnObjectFailure() method
+	** on the callback object.  Otherwise, it will return S_OK. */
 	HRESULT HandleHR(HRESULT hr);
 };
